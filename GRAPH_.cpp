6e8f07476c90b569a44bc3053d209d89649baac3
VietCT04 <69303535+VietCT04@users.noreply.github.com>
2020-08-11 17:25:26 +0700

    Create GRAPH_.cpp

---
+++ GRAPH_.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+const int N = 4e5 + 5;
+vector<pair<ll, ll>> node[N];
+ll vis[N], tin[N], low[N], e[N];
+ll timer = 0, ans = 0;
+unordered_map<ll, ll> mem;
+void dfs(ll u, ll p){
+        vis[u] = true;
+        tin[u] = low[u] = timer++;
+        ll child = 0;
+        for (auto to : node[u]){
+                if (e[to.second]) continue;
+                if (vis[to.first]){
+                        low[u] = min(low[u], tin[to.first]);
+                } else {
+                        e[to.second] = 1;
+                        dfs(to.first, 1);
+                        low[u] = min(low[u], low[to.first]);
+                        if (low[to.first] > tin[u]) ans++;
+                        if (low[to.first] >= tin[u] && p != 0){
+                                mem[u] = 1;
+                        }
+                        child++;
+                }
+        }
+        if (p == 0 && child > 1) mem[u] = 1;
+}
+int main(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        if (fopen("GUARDS.INP", "r")){
+                freopen("GUARDS.INP", "r", stdin);
+                freopen("GUARDS.OUT", "w", stdout);
+        }
+        ll n, m;
+        cin >> n >> m;
+        ll cnt = 0;
+        for (int i = 1; i <= m; i++){
+                ll u, v;
+                cin >> u >> v;
+                cnt++;
+                node[u].push_back({v, cnt});
+                node[v].push_back({u, cnt});
+        }
+        for (int i = 1; i <= n; i++){
+                if (!vis[i]){
+                        dfs(i, 0);
+                }
+        }
+        cout << mem.size() << " " << ans;
+}
+/*
+*/
