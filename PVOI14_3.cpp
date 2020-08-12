bb3b4d6754a1ef7b9ea262eb824e66cb95ef66d8
VietCT04 <69303535+VietCT04@users.noreply.github.com>
2020-08-12 16:39:37 +0700

    Update PVOI14_3.cpp

--- PVOI14_3.cpp
+++ PVOI14_3.cpp
@@ -1,13 +1,22 @@
+// code 100đ trên cf, 95đ spoj
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define ll int
 #define endl '\n'
 const int N = 1e6 + 5;
-double eps = 1e-3;
-double d[1005];
-ll u[10005], v[10005], c[10005];
+long double eps = 1e-3;
+long double d[1005];
+ll u[10005], v[10005], c[10005], par[1005];
+bool Ok(int x, int y) {
+    while (x != 1){
+        x = par[x];
+        if (x == 0) return false;
+        if (x == y) return true;
+    }
+    return false;
+}
 int main(){
         ios_base::sync_with_stdio(false);
         cin.tie(0);
@@ -20,19 +29,26 @@ int main(){
         for (int i = 1; i <= m; i++){
                 cin >> u[i] >> v[i] >> c[i];
         }
-        double l = 1, h = 1e10;
+        long double l = 1, h = 1e10;
         while ((h - l) > eps){
-                double mid = (l + h) / 2;
+                long double mid = (l + h) / 2;
                 for (int i = 1; i <= n; i++) d[i] = 0;
+                for (int i = 1; i <= n; i++) par[i] = -1;
                 ll x = -1;
                 for (int i = 1; i <= n; i++){
                         x = -1;
                         for (int j = 1; j <= m; j++){
                                 if (d[v[j]] > (d[u[j]] + c[j] - mid)){
                                         d[v[j]] = d[u[j]] + c[j] - mid;
+                                        if (Ok(u[j], v[j])){
+                                                x = 156;
+                                                break;
+                                        }
+                                        par[v[j]] = u[j];
                                         x = v[j];
                                 }
                         }
+                        if (x == 156) break;
                 }
                 if (x == -1) l = mid + eps;
                 else h = mid - eps;
